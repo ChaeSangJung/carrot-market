33d09b0aa91c0429f482abcb42db0a6c20548883
che <shtingstar@hanmail.net>
2022-03-30 17:53:37 +0900

    9.6

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Serverless Carrot Market Clone using NextJS, Tailwind, Prisma, PlanetScale and Cloudflare.
 
-#9.2
+#9.4 
+  - 서버 다시 시작 : process.env 환경 변수 읽어야 함
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,14 @@
 import client from "@libs/server/client";
 import withHandler, {ResponseType} from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
 
-async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler (
+  req:NextApiRequest, 
+  res:NextApiResponse<ResponseType>
+){
   const { phone, email } = req.body;
   const user = phone ? { phone: +phone } : email ? {email} : null;
   if(!user) return res.status(400).json({ok: false});
@@ -23,9 +29,18 @@ async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
       },
     },
   });
+  // if(phone) {
+  //   const message = await twilioClient.messages.create({
+  //     messagingServiceSid: process.env.TWILIO_MSID,
+  //     to: process.env.MY_PHONE!,
+  //     body: `Your login token is ${payload}`
+  //   });
+  //   console.log(message)
+  // }
   return res.json({
     ok: true,
   });
 }
+
 
 export default withHandler("POST", handler);
