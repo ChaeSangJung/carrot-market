dc689f6f85ec22fa49fa641dcbd2494bd098f6f9
che <che@Jungui-iMac.local>
2022-03-30 14:46:46 +0900

    9-2

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,18 +4,61 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler (req:NextApiRequest, res:NextApiResponse){
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
+  /* if (email) { // email을 가진 유저 찾기
+    user = await client.user.findUnique({
+      where: {
+        email:email,
+      },
+    });
+    if (user) console.log("found it.");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("found it.");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  } */
   return res.status(200).end();
 }
 
